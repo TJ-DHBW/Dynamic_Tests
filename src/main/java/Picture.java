3cbff18aa32c8d4b631c595326faea77e8f70042
Flash3r01 <tim-johmann@web.de>
2020-10-22 17:53:08 +0200

    added Picture constructor

--- src/main/java/Picture.java
+++ src/main/java/Picture.java
@@ -1,5 +1,14 @@
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 public class Picture {
     private int faceID;
     private char[][] content;
     private long nanoTimeStamp;
+
+    Picture(int faceID, char[][] content){
+        this.faceID = faceID;
+        this.content = content;
+        this.nanoTimeStamp = LocalDateTime.now().toEpochSecond(ZoneOffset.ofHours(1));
+    }
 }
